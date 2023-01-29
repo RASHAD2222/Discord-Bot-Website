dd8c178d2c168a1084d31723553e04e5e68a3071
RASHAD2222 <110339272+RASHAD2222@users.noreply.github.com>
2023-01-29 22:31:54 +0200

    Update config.js

--- config.js
+++ config.js
@@ -38,10 +38,10 @@ const config = {
     // You can added more features if need add
   ],
   //Bot Features
-  serverCount: 12, //-  - Your
+  serverCount: 13, //-  - Your
   memberCount: 239, //--   - Bot
   executedCommand: 50, //--  - Stats
-  availableCommand: 30, //---   - Here
+  availableCommand: 40, //---   - Here
   //Do not change if you don't know about them
   faceBookLogo: (
     <BsFacebook className="h-10 w-10 text-[#4267B2] hover:scale-125 easeIn cursor-pointer" />
