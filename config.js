9ec3436a5db7d73f13cbabce72887229f576291c
RASHAD2222 <110339272+RASHAD2222@users.noreply.github.com>
2023-01-29 12:28:28 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ import { MdAddModerator } from "react-icons/md";
 import { SiProbot } from "react-icons/si";
 import { TbMessage2Code } from "react-icons/tb";
 const config = {
-  name: "ProBot",
+  name: "music bot",
   //BotName
   inviteLink:
     "https://discord.com/api/oauth2/authorize?client_id=1041284784955068467&permissions=8&scope=bot%20applications.commands",
@@ -47,7 +47,7 @@ const config = {
     <BsFacebook className="h-10 w-10 text-[#4267B2] hover:scale-125 easeIn cursor-pointer" />
   ),
   discordLogo: (
-    <FaDiscord className="h-10 w-10 text-[#738adb] hover:scale-125 easeIn cursor-pointer" />
+    <FaDiscord className="h-10 w-10 text-[#738adb] hovaer:scale-125 easeIn cursor-pointer" />
   ),
   youTubeLogo: (
     <BsYoutube className="h-10 w-10 text-[#FF0000] hover:scale-125 easeIn cursor-pointer" />
